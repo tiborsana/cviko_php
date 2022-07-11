30b352b322cdd3d846570b53156cd1f75f0187c2
tiborsana <104055586+tiborsana@users.noreply.github.com>
2022-07-11 09:39:11 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="UTF-8">
+</head>
+<body>
+<form method="POST" action="form02.php">
+	Text otázky:
+	<textarea name="popis"></textarea>
+	<input type="submit" value="Vložit" >
+</form>
+
+</body>
+</html>
